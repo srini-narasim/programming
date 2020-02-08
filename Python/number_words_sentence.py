090f61a37ad471763285f432000ae1df2424801c
Srini Narasimharajan <srcalny72@gmail.com>
2020-02-08 00:59:39 -0500

    Calculates the number of words in a sentence entered by the user.

---
+++ Python/number_words_sentence.py
@@ -0,0 +1,6 @@
+#Write a program that calculates the number of words in a sentence
+#entered by the user
+
+phrase = input("Enter a phrase: ")
+new_phrase = phrase.split()
+length_of_phrase = len(new_phrase)
